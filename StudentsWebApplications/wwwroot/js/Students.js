fefd2a9a124e03dada336819d30b53c6e9f9c217
staticvoid-1 <53930927+staticvoid-1@users.noreply.github.com>
2019-08-21 08:51:47 +0300

    Web sayfasına butın,modal page eklendi ve click hareketine tepki sağlandı.

---
+++ StudentsWebApplications/wwwroot/js/Students.js
@@ -0,0 +1,41 @@
+﻿function onloadEventHandler() {
+
+
+    var studentList = getStudentList();
+
+
+
+}
+
+function getStudentList() {
+    $.ajax({
+        url: 'http://localhost:62885/api/student'
+    }).done(function (data, textStatus, xhr) {
+        //alert(data);
+        //console.log(data);
+
+        //alert(data[0].name + " ve " + data[1].name);
+        console.log(Object.values(data));
+
+        fillStudentTable(data);
+        return data;
+    });
+
+}
+
+function fillStudentTable(data) {
+
+    var tBody = document.getElementById("studentTableBody");
+
+    for (i = 0; i < data.length; i++) {
+        var tableRow = document.createElement('tr');
+        tableRow.innerHTML =
+            "<td>" + data[i].name + "</td>" +
+            "<td>" + data[i].surname + "</td>" +
+            "<td>" + data[i].schoolId + "</td>";
+
+        tBody.appendChild(tableRow);
+
+    }
+
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -29,7 +29,7 @@ namespace WebApplication1
         {
             services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
              {
-                 builder.WithOrigins("http://localhost:62885").AllowAnyMethod().AllowAnyHeader();
+                 builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
              }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IStudentsServices>(new StudentServices());
