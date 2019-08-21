7f546038918b0b6ddf48a702054bf3a7b3d6c1c7
staticvoid-1 <53930927+staticvoid-1@users.noreply.github.com>
2019-08-21 08:50:48 +0300

    Console projesine Post metodu eklendi.

--- SampleConsoleClient/Program.cs
+++ SampleConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SampleConsoleClient.Services;
+using StudentsApi.Core.Models;
 using System;
 
 namespace SampleConsoleClient
@@ -8,8 +9,47 @@ namespace SampleConsoleClient
         static void Main(string[] args)
         {
            StudentServices studentService=new StudentServices();
-            var students = studentService.GetStudentList();
-            Console.WriteLine(students.Count);
+           var students = studentService.GetStudentList();
+            
+
+            Console.WriteLine("İsim giriniz:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyisim giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Okul no giriniz:");
+            string schoolIdStr = Console.ReadLine();
+
+            int schoolId;
+
+            try
+            {
+               schoolId = int.Parse(schoolIdStr);
+            }
+            catch
+            {
+                schoolId = -1;
+                Console.WriteLine("Girilen okul nosu geçerli değildir.");
+            }
+           
+
+            var student = new Student()
+            {
+                Name = name,
+                Surname = surname,
+                SchoolId = schoolId
+            };
+           ResponseResult studentAddingResult = studentService.AddStudent(student);
+            if (studentAddingResult.IsSuccessful)
+            {
+                Console.WriteLine("Öğrenci eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("İşlem başarısız oldu :"+studentAddingResult.Message);
+            }
+               
         }
     }
 }
--- SampleConsoleClient/Services/StudentServices.cs
+++ SampleConsoleClient/Services/StudentServices.cs
@@ -19,14 +19,38 @@ namespace SampleConsoleClient.Services
             if (response.IsSuccessful)
             {
               string json= response.Content;
-               return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Student>>(json);
+              var studentList= Newtonsoft.Json.JsonConvert.DeserializeObject<List<Student>>(json);
+              return studentList;
             }
             else
             {
                 return null;
             }
+            
+            
+        }
 
+        public ResponseResult AddStudent(Student student)
+        {
+            RestClient client = new RestClient("https://localhost:44343/api/student");
 
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Charset", "utf-8");
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(student);
+            request.AddJsonBody(json);
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                return new ResponseResult(true);
+            }
+            else
+            {
+                return new ResponseResult(false,response.ErrorMessage);
+            }
         }
     }
 }
