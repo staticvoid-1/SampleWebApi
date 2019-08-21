1513037d37c21a021093d7acb14556823458f984
staticvoid-1 <53930927+staticvoid-1@users.noreply.github.com>
2019-08-21 08:50:11 +0300

    Console projesi için ResponseResult modeli oluşturuldu.

---
+++ StudentsApi.Core/Models/ResponseResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsApi.Core.Models
+{
+    public class ResponseResult
+    {
+        public bool IsSuccessful { get; set; }
+        public string Message { get; set; }
+        public ResponseResult(bool isSuccesful)
+        {
+            this.IsSuccessful = isSuccesful;
+
+        }
+        public ResponseResult(bool isSuccessful,string message)
+        {
+            this.IsSuccessful = isSuccessful;
+            this.Message = message;
+        }
+
+
+    }
+}
